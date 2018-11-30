48f738824ef1cd163b04b5d58e0bd9b734a77e3e
Alan Chu <thechutrain@users.noreply.github.com>
2018-11-30 18:27:44 -0500

    02 server (#4)
    * using node v10
    
    * using double quotes instead now
    
    * lint only js files, random quote producing server
    
    * added a few more quotes
    
    * specifying port by env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const http = require("http");
+const quotes = require("./random_quotes");
+
+const PORT = process.env.PORT || 7000;
+
+http
+	.createServer((req, res) => {
+		const randIdx = Math.floor(Math.random() * quotes.length);
+		const quoteObj = quotes[randIdx];
+
+		res.writeHead(200, { "Content-type": "text/plan" });
+		res.write(`"${quoteObj.text}" -- ${quoteObj.author}`);
+		res.end();
+	})
+	.listen(PORT);
