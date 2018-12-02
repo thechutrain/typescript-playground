cd37aef30081edd45d5d1d1539e1f1149c1f0d2b
Alan Chu <thechutrain@users.noreply.github.com>
2018-12-01 22:56:13 -0500

    01 sprint (#2)
    * yarn lock file for init download
    
    * weird that when I import data json file, adds src/ dir ... removed it
    
    * changed tslint config
    
    * express server basics
    
    * added express and fixed the dev environments
    
    * only printing out things based on NODE_ENV
    
    * added express types
    
    * removing the types property, since only those will be included, default will use node_modules/@types
    
    * added dotenv and configuration for environment
    
    * testing env vars
    
    * removed the dev & prod env files
    
    * dont track env files

---
+++ config/index.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+switch (process.env.NODE_ENV.toUpperCase()) {
+	case "DEVELOPMENT":
+		require("dotenv-safe").config({
+			path: path.join(__dirname, "./.env.development"),
+			sample: path.join(__dirname, "./.env.example")
+		});
+		break;
+
+	case "PRODUCTION":
+		require("dotenv-safe").config({
+			path: "./env.production",
+			sample: "./env.example"
+		});
+		break;
+
+	default:
+		throw new Error(
+			`process.env.NODE_ENV of "${process.env.NODE_ENV}" has no configuration`
+		);
+		break;
+}
+
+// Testing
+console.log(process.env.FOO);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express = require("express");
+import morgan = require("morgan");
+
+//  ==== Load Environmental Variables ===
+// tslint:disable-next-line:no-var-requires
+require("../config");
 const app = express();
 
 const PORT = process.env.PORT || 7000;
 
+app.use(morgan("common"));
+
+app.get("/api", (req, res, next) => {
+	res.send("api route to be made");
+});
+
 app.get("/*", (req, res, next) => {
-	res.send("hi");
+	res.send("Only routes that are processed are API & AUTH");
+
 });
 
 app.listen(PORT, e => {
